c28a72bf762c826c74a801013660990aea6bca21
herber30 <herbert.oliveira07@outlook.com>
2024-04-16 19:12:50 -0300

    Adicao de Js dos graficos
    Verificando formatação e também como puxar dados armazenados para os graficos

---
+++ assets/js/graficos.js
@@ -0,0 +1,62 @@
+let expensesData = [1000, 500, 200]
+let incomeData = [2000, 1500]
+
+const despesasCanvas = document.getElementById('despesasChart')
+const receitasCanvas = document.getElementById('receitasChart')
+
+const despesasChart = new Chart(despesasCanvas, {
+  type: 'bar',
+  data: {
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    datasets: [{
+      label: 'Despesas de Abril',
+      data: expensesData,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+      barRadius: 0.4,
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+})
+
+const receitasChart = new Chart(receitasCanvas, {
+  type: 'bar', 
+  data: {
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'], 
+    datasets: [{
+      label: 'Receitas de Abril',
+      data: incomeData,
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1,
+      barRadius: 0.4,
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+})
+
+function updateCharts(expenses, incomes) {
+  expensesData = expenses
+  incomeData = incomes
+  despesasChart.data.datasets[0].data = expenses
+  receitasChart.data.datasets[0].data = incomes
+  despesasChart.update()
+  receitasChart.update()
+}
